b13f75c600d2bc930d9b9e197c76bdde9480231b
Hannibal Moulvad <Hannibal Moulvad>
2015-10-19 14:18:22 +0200

    Fjernet kommentarer

--- src/game/DiceCup.java
+++ src/game/DiceCup.java
@@ -25,7 +25,7 @@ public class DiceCup {
 			return true;
 		}
 		else {
-			return false;// ALEX & HMOULVAD ER EN SVANS sdasd
+			return false;//
 		}		
 	}
 	//Method to return the sum of die1 and die2	
